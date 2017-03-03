436040afc6a992cb1a88a00e76a2deaff73b2ca2
DKMitt <dkmitt@sbcglobal.net>
2017-03-02 23:45:45 -0600

    updating pseudo coding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
 
-
 /* Pseudo Coding
 
 button starts the game with countdown timer.
+	* Game initializes
+	* timer gets set and starts
 
 viewer answers questions (only 1 answer per question) capture those answers.
-
+	* user answers questions
+	
 game ends when viewer clicks done button within time limit or time runs out.
+	* on button click checks answers
+	* timers runs out checks answers
+	* games ends
 
-display the results to the user of the quantity of correct, incorrect and unanswered questions and win/lose message.
+display the results to the user of the quantity of correct, incorrect and 
+unanswered questions and win/lose message.
+	* displays results of wins
+	* displays results of loses
+	* displays results of unanswered
+	* displays win/lose message
 
 */
 
 
+	var wins = 0;
+	var losses = 0;
+	
+	
+	var 
