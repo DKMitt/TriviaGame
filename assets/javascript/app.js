3154947a00737358039f209e414170ad17d5edc0
DKMitt <dkmitt@sbcglobal.net>
2017-03-03 01:14:03 -0600

    adding variables and Q&A's

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,8 +23,79 @@ unanswered questions and win/lose message.
 */
 
 
-	var wins = 0;
+    var wins = 0;
 	var losses = 0;
+    var winAudio = new Audio('assets/audio/win.wav');
+    var loseAudio = new Audio('assets/audio/lose.wav');
 	
 	
-	var 
+	var iqac [{
+		id: "qa1",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+		
+	}, {
+		id: "qa2",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa3",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa4",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa5",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa6",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa7",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa8",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa9",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}, {
+		id: "qa10",
+		question: "",
+		answer: "",	
+		correctAnswer: "",
+	}]
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
